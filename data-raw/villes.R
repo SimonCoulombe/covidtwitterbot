b0e1fb72355424fc3b9ea3d90504d26d0af05ad0
Simon Coulombe <simoncoulombe@gmail.com>
2020-11-17 23:09:00 -0500

    lat-lon des villes

---
+++ data-raw/villes.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(ggmap)
+library(sf)
+ggmap::register_google(key = Sys.getenv("googlemap_api_key"),
+                      account_type="premium")
+cities <- c("Montréal", "Sherbrooke", "Quebec city", "Trois-Rivières", "Gatineau", "Saguenay", "Rivière-du-Loup", "Saint-Georges-de-Beauce")
+latlongs <- geocode(cities)
+villes <- tibble(cities, lon = latlongs$lon, lat = latlongs$lat)
+
+
+readr::write_csv(villes, "data-raw/villes.csv")
+villes <- st_as_sf(villes, coords = c("lon", "lat"), remove = FALSE, crs = 4326, agr = "constant")
+usethis::use_data(villes)
