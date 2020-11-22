dce3e8fd031221143d0bf51b85ae8803da6f9a4e
Simon Coulombe <simoncoulombe@gmail.com>
2020-11-22 13:57:37 -0500

    add library(dplyr) pour filter) git push

--- analysis/walkthrough.R
+++ analysis/walkthrough.R
@@ -1,15 +1,20 @@
 #This script creates all the figures used by the bot.
 library(ggplot2)
+library(dplyr)
 library(covidtwitterbot)
+
 Sys.setlocale("LC_TIME", "fr_CA.UTF8")
 ## demo some data functions, not necessary here
 
+message("Get INSPQ data")
 hist <- get_inspq_covid19_hist()
 
-
+message("graph hosts, test")
 graph_deces_hospit_tests()
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/quebec_deces_si.png")
 
+
+message("graph quebec cases by pop, test")
 graph_quebec_cas_par_region()
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/quebec_cases_by_pop.png")
 
@@ -21,6 +26,7 @@ myggsave(filename = "~/git/adhoc_prive/covid19_PNG/heatmap_age.png" , width = 14
 
 
 # créer la carte des cas par RLS de la semaine passée
+message("graph rls")
 rls_data <- get_rls_data()
 
 graph_quebec_cas_par_rls_heatmap(rls_data = rls_data)
@@ -32,7 +38,7 @@ myggsave(filename = "~/git/adhoc_prive/covid19_PNG/carte_rls_cases.png" , width 
 carte <- carte_rls_zoom_montreal(rls_data = rls_data)
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/carte_rls_cases_zoom_montreal.png" , width = 12, height =10)
 
-
+message("graph css")
 css_last_week <- get_css_last_week(rls_data)
 
 carte_css(css_last_week)
@@ -42,7 +48,7 @@ graph_css_bars(css_last_week)
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/css_cases_bars.png" , width = 14, height =14)
 
 
-
+message("type_pop_anything_quebec")
 temp <- type_par_pop_anything_quebec(type = region, variable = hos_quo_tot_n  )
 
 ggplot(data = temp,aes(x= date_report, y = avg_hos_quo_tot_n_last7_per_1M))+
@@ -60,6 +66,8 @@ ggplot(data = temp,aes(x= date_report, y = avg_hos_quo_tot_n_last7_per_1M))+
 
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/quebec_hospit_by_pop.png" )
 
+
+message("type_pop_anything_quebec2")
 temp <- type_par_pop_anything_quebec(type = region, variable = dec_quo_tot_n  )
 
 ggplot(data = temp,aes(x= date_report, y = avg_dec_quo_tot_n_last7_per_1M))+
@@ -78,7 +86,7 @@ ggplot(data = temp,aes(x= date_report, y = avg_dec_quo_tot_n_last7_per_1M))+
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/quebec_deces_by_pop.png" )
 
 
-
+message("type_pop_anything_quebec3")
 
 temp <- type_par_pop_anything_quebec(type = region, variable = psi_quo_tes_n  )
 
@@ -100,9 +108,10 @@ myggsave(filename = "~/git/adhoc_prive/covid19_PNG/quebec_tests_by_pop.png" )
 
 # Pourcentage de positivité québec
 
-
+message("pourcentage de positivité québec")
 
 temp <- type_par_pop_anything_quebec(type = region, variable = psi_quo_pos_t  )
+
 temp  %>%
   filter(groupe == "Ensemble du Québec") %>%
   ggplot(aes(x=date_report, y = psi_quo_pos_t))+
@@ -110,7 +119,7 @@ temp  %>%
   theme_simon()+
   labs(
     title = "Pourcentage de positivité (Ensemble du Québec",
-    subtitle = paste0("dernière mise à jour le ", format(max(hist$date, na.rm= TRUE), format = format_francais)),
+    subtitle = paste0("dernière mise à jour le ", format(max(temp$date_report, na.rm= TRUE), format = format_francais)),
     x = "Date de résultat du test",
     y = "Pourcentage de positivité",
     caption = "gossé par @coulsim"
@@ -118,6 +127,7 @@ temp  %>%
   scale_y_continuous(expand = c(0, 0))
 
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/quebec_positivite.png" )
+message("pourcentage de positivité québec par région")
 
 temp  %>%
   ggplot(aes(x=date_report, y = psi_quo_pos_t))+
@@ -125,7 +135,7 @@ temp  %>%
   theme_simon()+
   labs(
     title = "Pourcentage de positivité par région",
-    subtitle = paste0("dernière mise à jour le ", format(max(hist$date, na.rm= TRUE), format = format_francais)),
+    subtitle = paste0("dernière mise à jour le ", format(max(temp$date_report, na.rm= TRUE), format = format_francais)),
     x = "Date de résultat du test",
     y = "Pourcentage de positivité",
     caption = "gossé par @coulsim"
@@ -137,7 +147,7 @@ temp  %>%
 myggsave(filename = "~/git/adhoc_prive/covid19_PNG/quebec_positivite_par_region.png" )
 
 
-
+message("pourcentage de positivité québec par âge")
 temp <- type_par_pop_anything_quebec(type = groupe_age, variable = psi_quo_pos_t  )
 
 temp  %>%
@@ -146,7 +156,7 @@ temp  %>%
   theme_simon()+
   labs(
     title = "Pourcentage de positivité par groupe d'âge",
-    subtitle = paste0("dernière mise à jour le ", format(max(hist$date, na.rm= TRUE), format = format_francais)),
+    subtitle = paste0("dernière mise à jour le ", format(max(temp$date_report, na.rm= TRUE), format = format_francais)),
     x = "Date de résultat du test",
     y = "Pourcentage de positivité",
     caption = "gossé par @coulsim"
